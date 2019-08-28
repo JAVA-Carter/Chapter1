5840d83c148d1fab02b16a57210da4c516706f65
CamrenCarter <49414513+CamrenCarter@users.noreply.github.com>
2019-08-28 09:41:39 -0500

    update

---
+++ src/TablesAndChairs.java
@@ -0,0 +1,25 @@
+public class TablesAndChairs {
+    public static void main(String[] args) {
+        System.out.println("C                                                                                C");
+        System.out.println("C                                                                                C");
+        System.out.println("C                                                                                C");
+        System.out.println("C                                                                                C");
+        System.out.println("C                                                                                C");
+        System.out.println("C                                                                                C");
+        System.out.println("C                                                                                C");
+        System.out.println("C                                                                                C");
+        System.out.println("C                                                                                C");
+        System.out.println("C                                                                                C");
+        System.out.println("SEATSEATSEATSEATSEAT             TableTableTableTable             SEATSEATSEATSEAT");
+        System.out.println("L                  L             L                  L             L               L");
+        System.out.println("L                  L             L                  L             L               L");
+        System.out.println("L                  L             L                  L             L               L");
+        System.out.println("L                  L             L                  L             L               L");
+        System.out.println("L                  L             L                  L             L               L");
+        System.out.println("L                  L             L                  L             L               L");
+        System.out.println("L                  L             L                  L             L               L");
+        System.out.println("L                  L             L                  L             L               L");
+        System.out.println("L                  L             L                  L             L               L");
+
+    }
+}
---
+++ src/TacoTruck.java
@@ -0,0 +1,29 @@
+public class TacoTruck {
+    public static void main(String[] args){
+        System.out.println("ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss");
+        System.out.println("ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss");
+        System.out.println("sss     TTTTTTTT     AAAA       CCCCCCCCCCCC    OOOOOOOO     sss");
+        System.out.println("sss     TTTTTTTT    AAAAAA      CCCCCCCCCCCC    OOOOOOOO     sss");
+        System.out.println("sss       TTTT     AAAAAAAA     CCC             O      O     sss A");
+        System.out.println("sss       TTTT   AA        AA   CCC             O      O     sss   A ");
+        System.out.println("sss       TTTT   AAAAAAAAAAAA   CCC             O      O     sss     A");
+        System.out.println("sss       TTTT   AA        AA   CCC             O      O     sss       A");
+        System.out.println("sss       TTTT   AA        AA   CCCCCCCCCCC     OOOOOOOO     sss          A");
+        System.out.println("sss       TTTT   AA        AA   CCCCCCCCCCC     OOOOOOOO     sss            A");
+        System.out.println("ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss              A");
+        System.out.println("ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss----------------");
+        System.out.println("            O O O O                    O O O O O ");
+        System.out.println("            O O O O                    O O O O O ");
+        System.out.println("            O O O O                    O O O O O ");
+        System.out.println("            O O O O                    O O O O O ");
+        System.out.println("            O O O O                    O O O O O ");
+        System.out.println("            O O O O                    O O O O O ");
+
+
+
+
+    }
+
+
+
+}
