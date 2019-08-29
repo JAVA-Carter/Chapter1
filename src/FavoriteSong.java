cb661c79a78da92b168484f922db73f748def113
CamrenCarter <49414513+CamrenCarter@users.noreply.github.com>
2019-08-29 09:18:00 -0500

    commit and update random

---
+++ src/FavoriteSong.java
@@ -0,0 +1,9 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("It's all about you, girl");
+        System.out.println("On Your 16th birthday");
+
+
+    }
+
+}
---
+++ src/MovieQuote.java
@@ -0,0 +1,9 @@
+public class MovieQuote {
+    public static void main(String[] args) {
+        System.out.println("“May the Force be with you.”");
+        System.out.println("-Obi-Wan Kenobi");
+        System.out.println("Star Wars");
+        System.out.println("May 19th, 1999");
+
+    }
+}
