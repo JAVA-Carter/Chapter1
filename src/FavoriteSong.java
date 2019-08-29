b2bda47fd9363ed9af9ed05b7be6546f17d5da12
CamrenCarter <49414513+CamrenCarter@users.noreply.github.com>
2019-08-29 09:31:36 -0500

    update commit favorite songs

--- src/FavoriteSong.java
+++ src/FavoriteSong.java
@@ -2,6 +2,9 @@ public class FavoriteSong {
     public static void main(String[] args) {
         System.out.println("It's all about you, girl");
         System.out.println("On Your 16th birthday");
+        System.out.println("pay attention to you, girl");
+        System.out.println("everyone has to do what you say");
+        System.out.println("-From Boys Who Cry");
 
 
     }
